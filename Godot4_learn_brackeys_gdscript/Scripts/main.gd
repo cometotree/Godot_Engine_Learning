c7aac8d6ba5704d077795cf0fa9560dfe4f8bd1a
YSTONE\wzs_e <9moonlight@gmail.com>
2024-05-19 23:51:58 +0800

    no message

--- Godot4_learn_brackeys_gdscript/Scripts/main.gd
+++ Godot4_learn_brackeys_gdscript/Scripts/main.gd
@@ -1,19 +1,22 @@
 extends Node
 
+@export var health = 100
+@export var damage: int = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = "Hello, World!"
+	$Label.text = str(health)
 	$Label.modulate = Color.ROYAL_BLUE
 	print("Hello World!")
 
-
 func _input(event):
-	if event.is_action_pressed("my_action"):
+	if event.is_action_pressed("action_space"):
 		$Label.modulate = Color.BLUE_VIOLET
-	if event.is_action_released("my_action"):
+		health -= damage
+		if health > 0:
+			$Label.text = str(health)
+		else:
+			$Label.text = "You died!"
+		
+	if event.is_action_released("action_space"):
 		$Label.modulate = Color.ROYAL_BLUE
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
